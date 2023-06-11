0e1943b7df799a6883a37ffb09a017591eb8913c
RafayDev <imabdulrafayy@gmail.com>
2023-06-12 01:19:31 +0500

    null able leve;s and login fixes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Quiz;
+use App\Models\QuizQuestion;
+use App\Models\QuizAttempt;
 class UserController extends Controller
 {
     public function login(Request $request)
@@ -17,6 +21,12 @@ class UserController extends Controller
         $token = auth()->attempt($credentials);
         if (!$token) {
             return response()->json(['error' => 'Unauthorized'], 401);
+        }
+        if(auth()->user()->type == 'user'){
+            $quiz =new QuizAttempt();
+            $quiz->user_id = auth()->user()->id;
+            $quiz->quiz_id = auth()->user()->quiz_id;
+            $quiz->save();
         }
         return response()->json(['token' => $token, 'user' => auth()->user()]);
     }
